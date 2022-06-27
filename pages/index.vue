87562b18f5c3599ee116694603e8e903ae4c09b0
Owen Adsit <owenadsit@gmail.com>
2022-06-27 17:26:17 +0800

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <main>
+    <h1>Website in maintenence</h1>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo voluptas
+      doloremque quibusdam ipsam, accusantium corporis perspiciatis obcaecati
+      optio dolor. In sed tempora voluptates quis aperiam soluta impedit quasi,
+      maiores aspernatur!
+    </p>
+    <a target="_blank" href="mailto:">swasti@gmail.com</a>
+  </main>
+</template>
