e17e12fe47a171ae3a05950d7ba07dbfaf99630f
Owen Adsit <owenadsit@gmail.com>
2022-06-28 10:04:33 +0200

    title to swasti eco cottages, favicon set to nothing

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'swasti-temp',
+    title: 'Swasti Eco Cottages',
     htmlAttrs: {
       lang: 'en',
     },
@@ -14,7 +14,6 @@ export default {
 
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
       { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: true },
       { href: 'https://fonts.googleapis.com/css2?family=Gilda+Display&family=Lato:ital,wght@0,300;0,400;0,700;1,300;1,400&display=swap', rel: 'stylesheet' }
